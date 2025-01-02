493e1233e2e80e4bee00303456222255aff2c1f1
VishwadhwajTechExactly <vishwadhwaj.techexactly@gmail.com>
2025-01-02 18:04:24 +0530

    create add to cart feature

--- Groco/Controllers/Products.cs
+++ Groco/Controllers/Products.cs
@@ -14,15 +14,17 @@ namespace Groco.Controllers
         private readonly ApplicationDbContext _context;
 
         private readonly IWebHostEnvironment _environment;
+
+        private List<Product> products = new List<Product>();
         public Products(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             _context = context;
             _environment = environment;
         }
-        [Authorize(Roles =SD.Role_Admin)]
+        [Authorize(Roles = SD.Role_Admin)]
         public IActionResult Index()
         {
-            List<Product> products=_context.Products.ToList();
+            List<Product> products = _context.Products.ToList();
             return View(products);
         }
         [Authorize(Roles = SD.Role_Admin)]
@@ -36,7 +38,7 @@ namespace Groco.Controllers
             ProductViewModel productViewModel = new ProductViewModel
             {
                 Product = new Product(),
-                CategoryList=categories
+                CategoryList = categories
             };
             return View(productViewModel);
         }
@@ -44,11 +46,13 @@ namespace Groco.Controllers
         [HttpPost]
         public IActionResult Create(ProductViewModel productViewModel, IFormFile? file)
         {
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 string wwwRootPath = _environment.WebRootPath;
-                if (file != null) { 
-                    string fileName=Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
-                    string productPath=Path.Combine(wwwRootPath, @"Images\Product");
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"Images\Product");
 
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
@@ -58,7 +62,7 @@ namespace Groco.Controllers
                 }
                 _context.Products.Add(productViewModel.Product);
                 _context.SaveChanges();
-                return RedirectToAction("Index","Products");
+                return RedirectToAction("Index", "Products");
             }
             return View(productViewModel);
         }
@@ -71,7 +75,8 @@ namespace Groco.Controllers
             }
             var productFromDb = _context.Products.Find(id);
             IEnumerable<SelectListItem> categoryList = _context.Categories
-                .Select(u => new SelectListItem {
+                .Select(u => new SelectListItem
+                {
                     Text = u.Name,
                     Value = u.Id.ToString()
                 });
@@ -88,9 +93,10 @@ namespace Groco.Controllers
         }
         [Authorize(Roles = SD.Role_Admin)]
         [HttpPost]
-        public IActionResult Edit(ProductViewModel productViewModel,IFormFile? file)
+        public IActionResult Edit(ProductViewModel productViewModel, IFormFile? file)
         {
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
 
                 Product productFromdb = _context.Products.Find(productViewModel.Product.ProductId);
                 if (file != null)
@@ -158,19 +164,73 @@ namespace Groco.Controllers
             {
                 return NotFound();
             }
-            Product productFromDb= _context.Products.Find(id);
-            if (productFromDb == null) { 
+            Product productFromDb = _context.Products.Find(id);
+            if (productFromDb == null)
+            {
                 return NotFound();
             }
             _context.Products.Remove(productFromDb);
             _context.SaveChanges();
             return RedirectToAction("Index", "Products");
         }
-        [Authorize(Roles =SD.Role_Customer)]
+        [Authorize(Roles = SD.Role_Customer)]
         public IActionResult CustomerIndex()
         {
-            List<Product> products=_context.Products.ToList();
+            List<Product> products = _context.Products.ToList();
             return View(products);
         }
+
+        [Authorize(Roles = SD.Role_Customer)]
+        public void AddToCart(int? id)
+        {
+            Product productFromDb = _context.Products.Find(id);
+            if (productFromDb == null)
+            {
+                return;
+            }
+            products.Add(productFromDb);
+        }
+
+        [Authorize(Roles = SD.Role_Customer)]
+        public IActionResult ShowCart()
+        {
+            Dictionary<Product,int> productDictionary = new Dictionary<Product,int>();
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (productDictionary.ContainsKey(products[i]))
+                {
+                    productDictionary[products[i]]++;
+                }
+                else
+                {
+                    productDictionary.Add(products[i], 1);
+                }
+            }
+            return View(productDictionary);
+        }
+        [Authorize(Roles = SD.Role_Customer)]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            return RedirectToAction("ShowCart", "Products");
+        }
+        [Authorize(Roles = SD.Role_Customer)]
+        public IActionResult SubtractProduct(Product product)
+        {
+            products.Remove(product);
+            return RedirectToAction("ShowCart", "Products");
+
+        }
+        [Authorize(Roles = SD.Role_Customer)]
+        public IActionResult RemoveProduct(Product product) {
+            for (int i = products.Count; i >= 0; i--) { 
+                if (products[i].ProductId == product.ProductId)
+                {
+                    products.RemoveAt(i);
+                }
+            }
+            return RedirectToAction("ShowCart", "Products");
+        }
     }
+    
 }
